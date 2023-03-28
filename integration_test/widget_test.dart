8fe2d8b84f92bcce6a008a4711f1250a5a34875e
Алексей <sodnomoff@gmail.com>
2023-03-28 23:51:40 +0800

    fix: use widget() method

--- integration_test/widget_test.dart
+++ integration_test/widget_test.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
@@ -11,27 +9,18 @@ import 'package:test_task_flutter/yellow_screen.dart';
 // Mock [Generator] class
 class MockGenerator extends Mock implements RandomNumberGenerator {}
 
-// Smart generator
-class SmartRandomNumberGeneratorImpl implements RandomNumberGenerator {
-  @override
-  int generate() => Random().nextInt(50);
-}
-
 const String randomNumberText = 'Случайное число';
 
 void main() {
   late MockGenerator generator;
-  late SmartRandomNumberGeneratorImpl smartGen;
   setUpAll(() {
     generator = MockGenerator();
-    smartGen = SmartRandomNumberGeneratorImpl();
   });
   group('Yellow widget test: ', () {
     testWidgets(
         "1. It's required to have yellow screen with yellow background"
         "and with a button called 'случайное число'"
         "and a back button", (tester) async {
-      when(() => generator.generate()).thenReturn(4);
       await tester.pumpWidget(
         MaterialApp(
           title: 'Flutter Demo',
@@ -64,38 +53,27 @@ void main() {
           theme: ThemeData(
             primarySwatch: Colors.blue,
           ),
-          home: YellowScreen(generator: smartGen),
+          home: YellowScreen(generator: generator),
         ),
       );
       // Check if there is a button 'случайное число'
       final Finder randomButton = find.widgetWithText(ElevatedButton, randomNumberText);
       expect(randomButton, findsOneWidget);
-      // Tap on [randomButton]
-      await tester.tap(randomButton);
-      await tester.pumpAndSettle();
 
-      final Finder textContainer = find.byKey(const Key(Keys.randomNumberContainerKey));
-      final Finder text = find.descendant(of: textContainer, matching: find.byWidgetPredicate((widget) {
-        final bool checkIfTextIsNumber =
-            widget is Text && widget.data != null && int.tryParse(widget.data!) != null;
-        if (!checkIfTextIsNumber) {
-          // It seems that text is not a number
-          return false;
-        }
-        // Check if the number in required limits
-        final int number = int.parse(widget.data!);
-        final bool numberWithinLimits = number >= 0 && number <= 49;
-        if (!numberWithinLimits) {
-          return false;
-        }
-        final Color? numberColor = widget.style?.color;
-        final bool requiredColor = numberColor != null && numberColor == blueColor;
-        if (!requiredColor) {
-          return false;
-        }
-        return true;
-      }));
-      expect(text, findsOneWidget);
+      for (int i = 0; i < 50; i++) {
+        when(() => generator.generate()).thenReturn(i);
+        // Tap on [randomButton]
+        await tester.tap(randomButton);
+        await tester.pumpAndSettle();
+
+        final Finder textContainer = find.byKey(const Key(Keys.randomNumberContainerKey));
+        final Finder text = find.descendant(of: textContainer, matching: find.byType(Text));
+
+        final Text textWidget = text.evaluate().single.widget as Text;
+        expect(textWidget.data, isNotNull);
+        expect(int.parse(textWidget.data!), i);
+        expect(textWidget.style?.color == blueColor, true);
+      }
     });
   });
 }
